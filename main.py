69ea5be4d57813b53209cdf6db0ab29215cee0bc
Excal <74227473+Esyottil@users.noreply.github.com>
2024-11-17 20:24:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import string
 import random
 import os
 import sys
-import threading  # изменено для Python 3
+import threading 
 import httplib2
 import time
+
+if not os.path.exists('Image'):
+    os.makedirs('Image')
 
 if len(sys.argv) < 2:
     sys.exit("\033[37mUsage: python3 " + sys.argv[0] + " (Number of threads)")
@@ -26,21 +29,22 @@ def scrape_pictures(thread_id):
             url += ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(3))
             url += ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(3))
         
-        url += '.jpg'  # добавляем расширение файла .jpg
+        url += '.jpg'
 
         filename = url.rsplit('/', 1)[-1]
+        file_path = os.path.join('Image', filename) 
 
         h = httplib2.Http('.cache' + str(thread_id))
         response, content = h.request(url)
 
-        if response.status == 200:  # проверяем статус ответа
-            with open(filename, 'wb') as out:
+        if response.status == 200:
+            with open(file_path, 'wb') as out:
                 out.write(content)
 
-            file_size = os.path.getsize(filename)
+            file_size = os.path.getsize(file_path)
             if file_size in INVALID:
                 print("[-] Invalid: " + url)
-                os.remove(filename)
+                os.remove(file_path)
             else:
                 print("[+] Valid: " + url)
         else:
@@ -58,6 +62,5 @@ for i in range(1, THREAD_AMOUNT + 1):
 
 print(f'Successfully started {THREAD_AMOUNT} threads.')
 
-# Делаем так, чтобы основной поток не завершался
 for thread in threads:
     thread.join()
